1abb48cd5eaa9ad1babcb36574ac597e6ca1c91d
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-24 13:59:13 -0500

    sequelize eagerloading error product get route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,23 @@ const sequelize = require('../../config/connection');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log("IS THIS WORKING");
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: ProductTag },
+      ]
+    });
+      console.log('Logging productData');
+      console.log(productData);
+      res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
