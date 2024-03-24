a9896e860ef86e347b5b796801688610d61cee18
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-24 13:20:58 -0500

    all routes for category working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,42 +36,46 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new category
+  //passes check
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
-    console.log('Logging Category Create');
-    console.log(categoryData);
   } catch (err) {
-    console.log('Logging error');
-    console.log(err);
     res.status(400).json(err);
   }
 });
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
-  const { categoryName } = req.body;
-  res.json("update route");
-  // try {
-  //   const categoryId = await Category.findByPk(req.params.id);
+  // passes check 
+  const categoryID = req.params.id;
+  const { category_name } = req.body;
+    
+    try {
+      const category = await Category.findByPk(categoryID);
+      if (!category) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      category.category_name = category_name;
+      await category.save();
 
-  //   if (!categoryId) {
-  //     res.status(404).json({ message: 'No category with this id found'});
-  //     return;
-  //   }
-  //   res.
-  // },
+      res.json(category);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  // passes check
   try {
     const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       },
     });
-
+    
     if (!categoryData) {
       res.status(404).json({ message: "No category found with this id" });
       return;
